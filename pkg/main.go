e3a3c55935446e21660dcf68a3ca5f4cfa831813
Michael Hrivnak <mhrivnak@redhat.com>
2018-06-14 17:34:09 -0400

    wip - uses the k8s API to find the right OwnerReference to use

--- pkg/main.go
+++ pkg/main.go
@@ -7,10 +7,15 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"k8s.io/api/admission/v1beta1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 // patch - represents a JSONPatch
@@ -20,9 +25,14 @@ type patch struct {
 	Value interface{} `json:"value"`
 }
 
+type session struct {
+	Clientset  *kubernetes.Clientset
+	TargetType string
+}
+
 // dumper - dumps the incoming request to stdout and also creates a patch for
 // the OwnerReferences
-func dumper(w http.ResponseWriter, r *http.Request) {
+func (s *session) dumper(w http.ResponseWriter, r *http.Request) {
 	dump, err := httputil.DumpRequest(r, true)
 	if err != nil {
 		panic(err)
@@ -36,50 +46,74 @@ func dumper(w http.ResponseWriter, r *http.Request) {
 	}
 
 	aReview := v1beta1.AdmissionReview{}
-
 	err = json.Unmarshal(data, &aReview)
 	if err != nil {
 		panic(err)
 	}
 
-	w.Header().Set("content-type", "application/json")
-
-	pt := v1beta1.PatchTypeJSONPatch
-	sp, err := makePatch(&aReview)
+	owner, err := s.getOwner(&aReview)
 	if err != nil {
 		panic(err)
 	}
 
 	wrapper := v1beta1.AdmissionReview{
 		Response: &v1beta1.AdmissionResponse{
-			UID:       aReview.Request.UID,
-			Allowed:   true,
-			Patch:     sp,
-			PatchType: &pt,
+			UID:     aReview.Request.UID,
+			Allowed: true,
 		},
+	}
+
+	// If an owner reference was found on the ServiceAccount, add a patch
+	if owner != nil {
+		sp, err := makePatch(owner)
+		if err != nil {
+			panic(err)
+		}
+
+		pt := v1beta1.PatchTypeJSONPatch
+		wrapper.Response.Patch = sp
+		wrapper.Response.PatchType = &pt
 	}
 
 	respBody, err := json.Marshal(wrapper)
 	if err != nil {
 		panic(err)
 	}
+	w.Header().Set("content-type", "application/json")
 	w.Write(respBody)
 }
 
-// makePatch - creates and returns a patch, encoded as a JSON byte array, based
-// on the passed-in review.
-func makePatch(aReview *v1beta1.AdmissionReview) ([]byte, error) {
-	owner := metav1.OwnerReference{
-		APIVersion: "v1alpha1",
-		Kind:       "Foo",
-		Name:       "example-foo",
-		UID:        types.UID("97388dad-6da8-11e8-a81d-847297420507"),
-	}
+func (s *session) getOwner(aReview *v1beta1.AdmissionReview) (*metav1.OwnerReference, error) {
+	api := s.Clientset.CoreV1()
+	parts := strings.Split(aReview.Request.UserInfo.Username, ":")
+	// If it's a ServiceAccount, the username will be in the form
+	// "system:serviceaccount:<namespace>:<name>"
+	if len(parts) == 4 && parts[0] == "system" && parts[1] == "serviceaccount" {
+		user, err := api.ServiceAccounts(parts[2]).Get(parts[3], metav1.GetOptions{})
+		if err != nil {
+			if errors.IsNotFound(err) {
+				return nil, nil
+			}
+			return nil, err
+		}
+		fmt.Printf("%v\n", user)
 
+		for _, oref := range user.OwnerReferences {
+			if oref.Kind == s.TargetType {
+				return &oref, nil
+			}
+		}
+	}
+	return nil, nil
+}
+
+// makePatch - creates and returns a patch, encoded as a JSON byte array, based
+// on the passed-in review. It will overwrite any existing OwnerReference.
+func makePatch(owner *metav1.OwnerReference) ([]byte, error) {
 	p := patch{
 		Op:    "add",
 		Path:  "/metadata/OwnerReferences",
-		Value: []metav1.OwnerReference{owner},
+		Value: []metav1.OwnerReference{*owner},
 	}
 	sp, err := json.Marshal([]patch{p})
 	if err != nil {
@@ -90,8 +124,22 @@ func makePatch(aReview *v1beta1.AdmissionReview) ([]byte, error) {
 }
 
 func main() {
-	http.HandleFunc("/", dumper)
-	err := http.ListenAndServeTLS(":4443", "certs/yyc.crt", "certs/yyc.key", nil)
+	// Setup an API connection in a "session"
+	home := os.Getenv("HOME")
+	configpath := filepath.Join(home, ".kube/config")
+	config, err := clientcmd.BuildConfigFromFlags("", configpath)
+	if err != nil {
+		log.Fatal("BuildConfigFromFlags: ", err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal("NewForConfig: ", err)
+	}
+	session := session{Clientset: clientset, TargetType: "Foo"}
+
+	// Serve the webhook endpoint
+	http.HandleFunc("/", session.dumper)
+	err = http.ListenAndServeTLS(":4443", "certs/yyc.crt", "certs/yyc.key", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
